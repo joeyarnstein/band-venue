dad9ea654559a5caf529d21d4bafd6746e80e3bc
Joey <joeyarnstein@gmail.com>
2016-05-13 19:42:50 -0700

    create functionality for retrieving name and id for instances of band

--- src/main/java/Band.java
+++ src/main/java/Band.java
@@ -15,4 +15,84 @@ public class Band {
     return id;
   }
 
+  public void save() {
+    try(Connection con = DB.sql2o.open()) {
+      String sql = "INSERT INTO bands (name) VALUES (:name);";
+      this.id = (int) con.createQuery(sql, true)
+        .addParameter("name", this.name)
+        .executeUpdate()
+        .getKey();
+    }
+  }
+
+  public static List<Band> all(){
+    String sql = "SELECT * FROM bands;";
+    try(Connection con = DB.sql2o.open()) {
+      return con.createQuery(sql).executeAndFetch(Band.class);
+    }
+  }
+
+  public static Band find(int id){
+    String sql = "SELECT * FROM bands WHERE id=:id;";
+    try(Connection con = DB.sql2o.open()) {
+      Band fetchedBand = con.createQuery(sql)
+        .addParameter("id", id)
+        .executeAndFetchFirst(Band.class);
+      return fetchedBand;
+    }
+  }
+
+  public void updateName(String name) {
+    try(Connection con = DB.sql2o.open()) {
+      String sql = "UPDATE bands SET name = :name WHERE id = :id;";
+      con.createQuery(sql)
+        .addParameter("name", name)
+        .addParameter("id", this.id)
+        .executeUpdate();
+    }
+    this.name = name;
+  }
+
+  // public void addVenue(String venue) {
+  //   Integer venue_id;
+  //   Integer relationshipAlreadyExistsChecker;
+  //   try(Connection con = DB.sql2o.open()) {
+  //     String sql = "SELECT id FROM venues WHERE name=:name;";
+  //     venue_id = con.createQuery(sql)
+  //       .addParameter("name", tag)
+  //       .executeAndFetchFirst(Integer.class);
+  //   }
+  //   if (venue_id != null) {
+  //
+  //     try(Connection con = DB.sql2o.open()){
+  //       String sql = "SELECT id FROM bands_venues WHERE venue_id=:venue_id AND band_id=:band_id;";
+  //       relationshipAlreadyExistsChecker = con.createQuery(sql)
+  //         .addParameter("venue_id", venue_id)
+  //         .addParameter("band_id", this.id)
+  //         .executeAndFetchFirst(Integer.class);
+  //     }
+  //
+  //     if (relationshipAlreadyExistsChecker == null) {
+  //       try(Connection con = DB.sql2o.open()) {
+  //         String sql = "INSERT INTO bands_venues (band_id, venue_id) VALUES (:band_id, :venue_id);";
+  //         con.createQuery(sql)
+  //           .addParameter("venue_id", venue_id)
+  //           .addParameter("band_id", this.getId())
+  //           .executeUpdate();
+  //       }
+  //     }
+  //
+  //   } else {
+  //     Venue newVenue = new Venue(venue);
+  //     newVenue.save();
+  //     try(Connection con = DB.sql2o.open()) {
+  //       String sql = "INSERT INTO bands_venues (venue_id, band_id) VALUES (:venue_id, :band_id);";
+  //       con.createQuery(sql)
+  //         .addParameter("venue_id", newVenue.getId())
+  //         .addParameter("band_id", this.getId())
+  //         .executeUpdate();
+  //     }
+  //   }
+  // }
+
 }
--- src/test/java/BandTest.java
+++ src/test/java/BandTest.java
@@ -12,4 +12,23 @@ public class BandTest {
     Band testBand = new Band("Iron Maiden");
     assertTrue(testBand instanceof Band);
   }
+  @Test
+  public void getName_returnsNameOfBand_true(){
+    Band testBand = new Band("Iron Maiden");
+    assertTrue(testBand.getName().equals("Iron Maiden"));
+  }
+  @Test
+  public void saveAndAll_savesToDatabaseAndReceivesId_true(){
+    Band testBand = new Band("Karnivool");
+    testBand.save();
+    Band savedBand = Band.all().get(0);
+    assertTrue(testBand.getId() == savedBand.getId());
+  }
+  @Test
+  public void find_returnsABandFromDatabase_true(){
+    Band testBandOne = new Band("James Brown");
+    testBandOne.save();
+    Band sameBandFromDatabase = Band.find(testBandOne.getId());
+    assertTrue(testBandOne.getId() == sameBandFromDatabase.getId());
+  }
 }
